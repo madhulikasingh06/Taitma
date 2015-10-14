f0a47a6fb3331782a0ecd036310c26c9cc1f1594
madhulikasingh06 <madhulikasingh06@gmail.com>
2015-10-14 13:05:41 +0800

    Added Committee member Details

--- commitee-members.php
+++ commitee-members.php
@@ -9,8 +9,163 @@
 
         <?php include_once "common/inner-nav-bar.php"; ?>
 
+
+	<div id="commitee-members"><!-- commitee-members div starts-->
+
+				 <div class="row"> 
+   	 
+					   	 <div class="col-sm-offset-1  col-sm-3">
+					             <h5><b>FOUNDER</b></h5>
+						             <p><b>Mr. Gulu M. Jhanger</b><br>
+										United Importers Corporation<br>
+										101, T. V. Industrial Estate, 248, Worli Road, Vorli <br>
+										Tel.: 24939739.24927227/66603008<br>
+										Fax: 24934487 | E-mail: <a href="mailto:gulu@unitedtoys.net.">gulu@unitedtoys.net.</a>
+									</p><br>
+									
+
+									<h5><b>PRESIDENT</b></h5>
+						             <p><b>Mr. Vivek G. Ghangiani</b><br>
+						             	Pleasantime Products, 101, T. V. Industrial Estate <br>
+						             	248. Worli Road, Worli, MUMBAI - 400 030<br>
+										Tel.: 24939739/24927227/66603008<br>
+										Fax: 24934487 | E-mail: <a href="mailto:vivekj@unitedtoys.net">vivekj@unitedtoys.net</a><br><br>
+									</p>
+
+									<h5><b>VICE PRESIDENTS</b></h5>
+									<p><b>(North Zone)</b>
+
+										<br/> Girnar International<br> 
+										18 Kamal Building, 1st Floor<br> 
+										Swadeshi Market, Sadar Bazar<br> 
+										Delhi - 110 006<br>
+										 Tel.: 23673320/2<br> 
+										 Fax: 42381179 <br> 
+										 E-mail: <a href="mailto:uatoys@gmail.com">uatoys@gmail.com</a>
+									</p>
+
+									<p><b>(South Zone)</b> <br> 
+										Mr. Arun Mammen<br> 
+										Funskool (India) Ltd.<br> 
+										Tarapore Towers, 6th Floor<br> 
+										326 Anna Salai<br> 
+										CHENNAI - 600 002<br> 
+										Tel.: 28520054 to 29<br> 
+										Fax: 28520141 / 0912<br> 
+										E-mail: <a href="mailto:john@funskool.co.in">john@funskool.co.in</a>										
+									</p>
+
+
+									
+							</div>
+
+					             <div class="col-sm-4">
+					              <h5><b>HONORARY EXECUTIVE SECRETARY/TREASURER</b></h5>
+					          		<p><b>Mr. Manish V. Kukreja</b><br>
+										Min Toys Pvt. Ltd. Laxmi Vijay Society<br>
+										Bldg.35, Unit 146, Laxmi Indl. Estate<br>
+										New Link Road, Andheri (E), MUMBAI - 400 053<br> 
+										Tel.26364115 / Mbl.: 9820062683 Fax: 26366049<br>
+										E-mail: <a href="mailto:manish@shinsei.in ">manish@shinsei.in </a>/ 
+										<a href="mailto:shinseitoys@gmail.com">shinseitoys@gmail.com</a>
+						          		<br><br></p>
+
+						          	<h5><b>MEMBERS </b></h5>
+						          	<p><b>Mr. Ashok Kumar Jain</b><br>
+						          		Chirantan Enterprise<br>
+										311/3 Ashirwad Industrial Estate, Ram Mandir Road,<br>
+										 Goregaon (W), MUMBAI - 400 104<br>
+										Tel.: 26763577/26763892 / 02502458861 <br>
+										0250-2458861(Vasai Fac.)<br>
+										Fax: 26763773 Mbl.: 9820086890<br>
+										E-mail:<a href="mailto:chirantan@mtnl.net.in"> chirantan@mtnl.net.in</a>
+										/<a href="mailto:jainashok92@yahoo.com">jainashok92@yahoo.com</a>
+						          	</p>
+
+						          	<p><b>Mr. Juzer A. Gabajiwala</b><br>
+						          		Tayebally Ebrahim &amp; Sons<br>
+						          		2 Gabajiwala Indl. Estate<br>
+						          		5 Makwana Road, Marol<br>
+						          		Mumbai - 400 059<br>
+										Tel.: 28504395 / Fax: 28593625<br>
+										Mbl.: 9820996852<br>
+										E-mail: <a href="mailto:peacock@peacocktoys.com">peacock@peacocktoys.com</a>					    
+						          	</p>
+
+						          	<p><b>Mr. Shabbir A. Gabajiwala</b><br>
+						          		Toys Box, 204 Zafryn Chambers, 2nd Floor, Sewri (E) <br>
+						          		MUMBAI - 400 016, Telefax: 24151119<br>
+										Mbl.: 9820084514 | Email:<a href="mailto:shabbir.gabajiwala@gmail.com">shabbir.gabajiwala@gmail.com</a> 
+
+						          		
+
+						          	</p>
+
+						          	<p><b>Mr. Anuj R. Mehta</b>	<br>					          		
+						          		Pegasus International<br>
+										A-311/312 Antop Hill warehousing Co. Ltd.<br>
+										Barkat Ali Road, Wadala (E), Mumbai - 400 037 <br>
+										Tel: 2417 2740/3698 |<br>
+										Fax: 24172746 | Mbl.: 9820475236<br>
+										E-mail:<a href="mailto:pegasustoys@gmail.com">pegasustoys@gmail.com</a>
+						          	</p>
+
+						            </div>
+					         
+
+
+
+					         <div class="col-sm-4">
+					              	<h5><b>(EVENTS ORGANISER)</b></h5>
+					              	<p><b>Mr. Vikram Goel</b><br>
+					              		Infotech Resources, 402 Sharda Chambers<br>
+					              		 New Marine Lines, MUMBAI - 400 020<br>
+										Mbl.: 9820063987 | E-mail:<a href="mailto:infotech@vsnl.in">infotech@vsnl.in</a> 
+					              	</p><br>
+
+
+								<h5><b>CO-OPTED MEMBERS</b></h5>
+					              	<p><b>Mr. Vijay Kumar Jain</b><br>
+					              		Speed Age Corporation<br>
+					              		602, Satyadev House, 6th Floor<br>
+					              		Veera Desai Road Extn., Andheri (W)<br>
+					              		MUMBAI - 400 053
+										Tel.: 26732153 / Fax: 26731439<br> 
+										Mbl.: 9820010491<br>
+										E-mail:<a href="mailto:vijayspeedage@gmail.com"> vijayspeedage@gmail.com</a>
+					              	</p>
+
+					              		<p><b>Mr. Moiz Z. Gabajiwala</b><br>
+					              		Zephyr Toymakers Pvt. Ltd.<br>
+										A421, ITC Indl. Area, MIDC<br>
+										Mahape<br>
+										NAVI MUMBAI - 400 706<br>
+										Tel.: 27780605/ 2485 - Fax: 27782484 <br>
+										Mbl.: 9870199862<br>
+										E-mail:<a href="mailto:gabajiz@gmail.com">gabajiz@gmail.com</a>
+					              	</p>
+
+
+
+
+					            </div>
+					         </div>
+		</div>
+
+
+
+
+	</div><!-- commitee-members div ends-->
+
+    
+
+
+   
+
       </div> <!--commitee-members-page div ends  -->
 
+
+   
 
 
 

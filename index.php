3a22601afb888a718ba3d147226fde524b6293b4
madhulikasingh06 <madhulikasingh06@gmail.com>
2015-10-14 14:18:51 +0800

    Changes for text
    saved the text as a part of images only

--- index.php
+++ index.php
@@ -82,13 +82,15 @@
 
      <div class="row">
             <div class="col-sm-offset-4  col-sm-7 home-text">
-              <p>THE ALL INDIA TOY MANUFACTURERS’ ASSOCIATION<br>
+              <!-- 
+<p>THE ALL INDIA TOY MANUFACTURERS’ ASSOCIATION<br>
               (TAITMA) was established in 1976 with the prime object of <br>fostering integrated and accelerated growth and development of<br>
               the toy industry in India, in a systematic and scientific manner,<br>
               and to exploit its export potential to earn valuable foreign<br>exchange for the country.</p>
 
               <p>The toy industry in India has tremendous potential to raise its<br> productivity, create employment and for all-round development of<br>the economy. The industry also presents vast potential for export<br> of toys, dolls, games and playthings. However, the toy industry<br>has not received adequate attention for its development.
                 
+ -->
               </p>
            </div>
            <div class="col-sm-1"></div>
